a20210826530b5cd411b24339c95982dd0f34fc8
darkhacker2005 <85599878+darkhacker2005@users.noreply.github.com>
2021-08-17 11:03:11 +0530

    Update d3d.js

--- plugins/d3d.js
+++ plugins/d3d.js
@@ -16,7 +16,7 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/text3dbox?apikey=${Config.ZEKS_API}&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'DARK KNIGHT BOT 🇱🇰 ➠ BY HITUWA'})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'DARK KNIGHT BOT 🇱🇰 ➠ BY HIRUWA'})
 
     }));
 }
@@ -29,7 +29,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/text3dbox?apikey=${Config.ZEKS_API}&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'DARK KNIGHT BOT 🇱🇰 ➠ BY HITUWA'})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'DARK KNIGHT BOT 🇱🇰 ➠ BY HIRUWA'})
 
     }));
     
