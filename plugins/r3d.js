38efa11be1f81c9f9c70f1e0c3db1de912129767
darkhacker2005 <85599878+darkhacker2005@users.noreply.github.com>
2021-08-17 10:04:05 +0530

    Update r3d.js

--- plugins/r3d.js
+++ plugins/r3d.js
@@ -10,26 +10,26 @@ const need = "type some word after command\n🗡🗡"
 
 if (Config.WORKTYPE == 'private') {
 
-    RAVANA.addCommand({ pattern: 'r3d ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+    RAVANA.addCommand({ pattern: 'd3d ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/text3dbox?apikey=${Config.ZEKS_API}&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'DARK KNIGHT BOT 🇱🇰 ➠ BY HITUWA'})
 
     }));
 }
 
 else if (Config.WORKTYPE == 'public') {
 
-    RAVANA.addCommand({ pattern: 'r3d ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+    RAVANA.addCommand({ pattern: 'd3d ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/text3dbox?apikey=${Config.ZEKS_API}&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'DARK KNIGHT BOT 🇱🇰 ➠ BY HITUWA'})
 
     }));
     
