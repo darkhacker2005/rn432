b8e9a943a89eb73a5aad4c6e1ef1e77601e2c907
darkhacker2005 <85599878+darkhacker2005@users.noreply.github.com>
2021-08-17 17:47:01 +0530

    Update and rename ranony.js to dhack.js

--- plugins/dhack.js
+++ plugins/dhack.js
@@ -6,30 +6,30 @@ const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys'
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-const need = "type some word after command\n🗡🗡"
+const need = "කමාන්ඩ් එකට පස්සේ වචනයක් ලබා දෙන්න\n 🇱🇰"
 
 if (Config.WORKTYPE == 'private') {
 
-    RAVANA.addCommand({ pattern: 'ranony?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+    RAVANA.addCommand({ pattern: 'dhack?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
         var ttinullimage = await axios.get(`https://lolhuman.herokuapp.com/api/ephoto1/anonymhacker?apikey=${Config.LOLUHUMAN_API}&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption:  'DARK KNIGHT BOT 🇱🇰 ➠ BY HITUWA '})
 
     }));
 }
 
 else if (Config.WORKTYPE == 'public') {
 
-    RAVANA.addCommand({ pattern: 'ranony ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+    RAVANA.addCommand({ pattern: 'dhack ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
         var ttinullimage = await axios.get(`https://lolhuman.herokuapp.com/api/ephoto1/anonymhacker?apikey=${Config.LOLUHUMAN_API}&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAPTION_KEY})
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption:  'DARK KNIGHT BOT 🇱🇰 ➠ BY HITUWA' })
 
     }));
     
